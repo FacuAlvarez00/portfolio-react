27766899194b58a174def66ce5bb5e297f1524a9
KernelOS <facucarp728@gmail.com>
2023-04-10 01:49:14 -0300

    finalizo social sidebar, mejoro estilos

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -4,6 +4,7 @@ import github from "../../assets/contact/github (2).svg"
 import linkedin from "../../assets/contact/linkedin (1).svg"
 import whatsapp from "../../assets/contact/whatsapp (1).svg"
 import message from "../../assets/contact/envelope-solid.svg"
+import instagram from "../../assets/contact/icons8-instagram.svg"
 import { UserAuth } from '../../context/AppContext'
 
 
@@ -32,9 +33,11 @@ const Contact = () => {
                     <a href='https://api.whatsapp.com/send?phone=541155617969' target="_Blank"><li><img src={whatsapp}/>+54 1155617969</li></a>
                     <a href='https://www.linkedin.com/in/facundoalvarezzz/' target="_Blank"><li><img src={linkedin}/>facundoalvarezzz</li></a>
                     <a href='https://github.com/FacuAlvarez00' target="_Blank"><li><img src={github}/>FacuAlvarez00</li></a>
+                    <a  target="_Blank"><li><img className='igIcon' src={instagram}/>facualvarezzz</li></a>
                     <a><li><img src={message}/>facundoalvarezzh@gmail.com</li></a>
+                    
                 </ul>
-            </div>
+            </div>  
 
         </div>
       
--- src/components/SideInfo/SideInfo.tsx
+++ src/components/SideInfo/SideInfo.tsx
@@ -1,25 +1,43 @@
-import React from 'react'
+
 import "./sideinfo.scss"
 import github from "../../assets/contact/github (2).svg"
 import linkedin from "../../assets/contact/linkedin (1).svg"
 import whatsapp from "../../assets/contact/whatsapp (1).svg"
 import instagram from "../../assets/contact/icons8-instagram.svg"
+import { useState } from "react"
+
 
 const SideInfo = () => {
+
+  const [asideBar, setasidebar] = useState<boolean>(true)
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 2000) {
+      setasidebar(false)
+      console.log("false")
+    }
+    else{
+      setasidebar(true)
+      console.log("true")
+    }
+ })
+
+
   return (
-    <div className='aside__socials'>
+    
+    <div className={asideBar? 'aside__socials' : "aside__socials-hide"}>
         <ul>
             <li><a><img src={linkedin}/></a></li>
             <li><a><img src={github}/></a></li>
             <li><a><img src={whatsapp}/></a></li>
             <li><a><img src={instagram}/></a></li>
 
-            <div className='aside__line'></div>
+            
             
         </ul>
 
-      
-
+        <div className='aside__line'></div>
+        
 
       
     </div>
